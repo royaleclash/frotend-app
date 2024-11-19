feee68012cbc69d0b575e66dfe6fa4a786423618
royaleclash <sincabeza90@gmail.com>
2024-11-19 07:34:35 -0500

    api de conexion

---
+++ js/api.js
@@ -0,0 +1,45 @@
+const API_URL="http://localhost:3000/api/Hamburger";
+
+//obtener todos las Hamburguesas
+
+export const getHamburger= async () => {
+    const response = await fetch(API_URL);
+    return response.json();
+
+}
+
+//obtener hamburguesa por ID
+
+export const getHamburgerByID = async (id) =>{
+    const response = await fetch (`${API_URL}/${id}`);
+};
+
+//crear una hamburguesa 
+
+export const addHamburger = async (hamburger) =>{
+    const response = await fetch (API_URL,{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(hamburger)
+    });
+    return response.json();
+};
+
+//actualizar una hamburguesa 
+
+export const updateHamburger = async (id,hamburger) =>{
+    const response = await fetch (`${API_URL}/${id}`,{
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(hamburger)
+    });
+    return response.json();
+};
+
+//borrar una hamburguesa 
+
+export const deleteHamburger = async (id) =>{
+    const response = await fetch (`${API_URL}/${id}`,{
+        method: "DELETE",
+    });
+};
